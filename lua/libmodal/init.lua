0b59dfa0fee68d54e99cfa59a607bebfa8a333da
Iron-E <code.iron.e@gmail.com>
2022-04-25 13:51:55 -0400

    feat(libmodal): allow `exit_char` in `libmodal.layer.enter`

--- lua/libmodal/init.lua
+++ lua/libmodal/init.lua
@@ -1,14 +1,24 @@
+-- TODO: this is the only `require` left which is done automatically by `libmodal`. There is probably a way to remove it.
+local Layer = require 'libmodal/src/Layer'
+
 return
 {
+	Layer = Layer,
 	layer =
 	{
 		--- Enter a new layer.
 		--- @param keymap table the keymaps (e.g. `{n = {gg = {rhs = 'G', silent = true}}}`)
-		--- @return function exit a function to exit the layer
-		enter = function(keymap)
-			local layer = require('libmodal/src/Layer').new(keymap)
+		--- @param exit_char nil|string a character which can be used to exit the layer from normal mode.
+		--- @return function|nil exit a function to exit the layer, or `nil` if `exit_char` is passed
+		enter = function(keymap, exit_char)
+			local layer = Layer.new(keymap)
 			layer:enter()
-			return function() layer:exit() end
+
+			if exit_char then
+				layer:map('n', exit_char, function() layer:exit() end, {})
+			else
+				return function() layer:exit() end
+			end
 		end
 	},
 
