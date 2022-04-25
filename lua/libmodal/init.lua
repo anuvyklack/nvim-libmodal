1d5e6df2c6fde8839535e2bd7ecba1c99c4d78c1
Iron-E <code.iron.e@gmail.com>
2022-04-25 14:19:13 -0400

    perf(libmodal): use __index to avoid `require`

--- lua/libmodal/init.lua
+++ lua/libmodal/init.lua
@@ -1,46 +1,64 @@
--- TODO: this is the only `require` left which is done automatically by `libmodal`. There is probably a way to remove it.
-local Layer = require 'libmodal/src/Layer'
-
-return
-{
-	Layer = Layer,
-	layer =
+-- TODO: remove the __index here after a period of time to let people remove `libmodal.Layer` from their configurations
+return setmetatable(
 	{
-		--- Enter a new layer.
-		--- @param keymap table the keymaps (e.g. `{n = {gg = {rhs = 'G', silent = true}}}`)
-		--- @param exit_char nil|string a character which can be used to exit the layer from normal mode.
-		--- @return function|nil exit a function to exit the layer, or `nil` if `exit_char` is passed
-		enter = function(keymap, exit_char)
-			local layer = Layer.new(keymap)
-			layer:enter()
+		layer =
+		{
+			--- Enter a new layer.
+			--- @param keymap table the keymaps (e.g. `{n = {gg = {rhs = 'G', silent = true}}}`)
+			--- @param exit_char nil|string a character which can be used to exit the layer from normal mode.
+			--- @return function|nil exit a function to exit the layer, or `nil` if `exit_char` is passed
+			enter = function(keymap, exit_char)
+				local layer = require('libmodal/src/Layer').new(keymap)
+				layer:enter()
 
-			if exit_char then
-				layer:map('n', exit_char, function() layer:exit() end, {})
-			else
-				return function() layer:exit() end
+				if exit_char then
+					layer:map('n', exit_char, function() layer:exit() end, {})
+				else
+					return function() layer:exit() end
+				end
+			end,
+
+			--- Create a new layer.
+			--- @param keymap table the keymaps (e.g. `{n = {gg = {rhs = 'G', silent = true}}}`)
+			--- @return libmodal.Layer
+			new = function(keymap)
+				return require('libmodal/src/Layer').new(keymap)
+			end,
+		},
+
+		mode =
+		{
+			--- Enter a mode.
+			--- @param name string the name of the mode.
+			--- @param instruction function|string|table a Lua function, keymap dictionary, Vimscript command.
+			enter = function(name, instruction, supress_exit)
+				require('libmodal/src/Mode').new(name, instruction, supress_exit):enter()
 			end
-		end
-	},
+		},
 
-	mode =
-	{
-		--- Enter a mode.
-		--- @param name string the name of the mode.
-		--- @param instruction function|string|table a Lua function, keymap dictionary, Vimscript command.
-		enter = function(name, instruction, supress_exit)
-			require('libmodal/src/Mode').new(name, instruction, supress_exit):enter()
-		end
+		prompt =
+		{
+			--- Enter a prompt.
+			--- @param name string the name of the prompt
+			--- @param instruction function|table<string, function|string> what to do with user input
+			--- @param user_completions table<string>|nil a list of possible inputs, provided by the user
+			--- @param supress_exit boolean|nil whether to stop the `<Esc>` key from quitting the mode
+			enter = function(name, instruction, user_completions, supress_exit)
+				require('libmodal/src/Prompt').new(name, instruction, user_completions, supress_exit):enter()
+			end
+		}
 	},
-
-	prompt =
 	{
-		--- Enter a prompt.
-		--- @param name string the name of the prompt
-		--- @param instruction function|table<string, function|string> what to do with user input
-		--- @param user_completions table<string>|nil a list of possible inputs, provided by the user
-		--- @param supress_exit boolean|nil whether to stop the `<Esc>` key from quitting the mode
-		enter = function(name, instruction, user_completions, supress_exit)
-			require('libmodal/src/Prompt').new(name, instruction, user_completions, supress_exit):enter()
-		end
+		__index = function(tbl, key)
+			if key ~= 'Layer' then
+				return rawget(tbl, key)
+			else
+				require('libmodal/src/utils/api').nvim_show_err(
+					require('libmodal/src/globals').DEFAULT_ERROR_TITLE,
+					'`libmodal.Layer` is deprecated in favor of `libmodal.layer`. It will work FOR NOW, but uncapitalize that `L` please :)'
+				)
+				return require 'libmodal/src/Layer'
+			end
+		end,
 	}
-}
+)
